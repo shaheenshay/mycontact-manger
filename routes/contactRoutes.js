a8a2132d949648937ba859f5fc329ce6adafee2d
shaheenshay <shaheen.shay@gmail.com>
2024-01-17 12:33:43 +0530

    error handling middle

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -8,14 +8,7 @@ const {
   getContacts,
 } = require("../controllers/contactController");
 
-router.route("/").get(getContacts);
-
-router.route("/").post(createContact);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
