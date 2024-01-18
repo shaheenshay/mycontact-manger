fba2d7e89be77d8e0a7eee2527dc1d73eb1618f6
shaheenshay <shaheen.shay@gmail.com>
2024-01-18 13:36:24 +0530

    login user create new conatct

--- models/contactModel.js
+++ models/contactModel.js
@@ -1,17 +1,23 @@
 const mongoose = require("mongoose");
 
-const contactSchema = mongoose.Schema({
+const contactSchema = mongoose.Schema(
+  {
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     name: {
-        type: String,
-        required: [true, "Please add the contact name"],
+      type: String,
+      required: [true, "Please add the contact name"],
     },
     email: {
-        type: String,
-        required: [true, "Please add the contact email address"],
+      type: String,
+      required: [true, "Please add the contact email address"],
     },
     phone: {
-        type: String,
-        required: [true, "Please add the contact phone number"],
+      type: String,
+      required: [true, "Please add the contact phone number"],
     },
   },
   {
