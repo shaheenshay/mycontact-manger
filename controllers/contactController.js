d6dcc8a9c49dab619f56e5f3e63d30954e41378e
shaheenshay <shaheen.shay@gmail.com>
2024-01-18 13:49:23 +0530

    bug fixes in CRUD operation

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -22,7 +22,7 @@ const createContact = asyncHandler(async (req, res) => {
     name,
     email,
     phone,
-    user_id: req.user.id
+    user_id: req.user.id,
   });
 
   res.status(201).json(contact);
@@ -50,6 +50,11 @@ const updateContact = asyncHandler(async (req, res) => {
     throw new Error("Contact not found");
   }
 
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contacts");
+  }
+
   const updatedContact = await Contact.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -68,8 +73,13 @@ const deleteContact = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Contact not found");
   }
-  
-  await Contact.deleteOne();
+
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contacts");
+  }
+
+  await Contact.deleteOne({ _id: req.params.id });
   res.status(200).json(contact);
 });
 
